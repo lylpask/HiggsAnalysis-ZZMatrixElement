662c4d54fbe4b61ea87a4a918653d2a82790f414
Giovanni <gpetruc@gmail.com>
2015-10-25 22:54:45 +0100

    - allow calling methods using the CMSSW LorentzVectors   (older interface kept for backwards compatibility) - update the KDs to what's currently in the Sync twiki   https://twiki.cern.ch/twiki/bin/view/CMS/HiggsZZ4l2015?rev=70   (plus fix float -> double when passing by reference)

--- PythonWrapper/interface/MEMCalculatorsWrapper.h
+++ PythonWrapper/interface/MEMCalculatorsWrapper.h
@@ -1,5 +1,7 @@
 #include <TLorentzVector.h>
 #include <vector>
+#include <string>
+#include "DataFormats/Math/interface/LorentzVector.h"
 struct MEMs;
 
 class MEMCalculatorsWrapper 
@@ -16,11 +18,24 @@ class MEMCalculatorsWrapper
 		   TLorentzVector Z1_lept2, int Z1_lept2Id,
 		   TLorentzVector Z2_lept1, int Z2_lept1Id,
 		   TLorentzVector Z2_lept2, int Z2_lept2Id) ;
+  Angles computeAngles(const math::XYZTLorentzVector & Z1_lept1, int Z1_lept1Id,
+		   const math::XYZTLorentzVector & Z1_lept2, int Z1_lept2Id,
+		   const math::XYZTLorentzVector & Z2_lept1, int Z2_lept1Id,
+		   const math::XYZTLorentzVector & Z2_lept2, int Z2_lept2Id) ;
+
 
   void  computeAll(TLorentzVector Z1_lept1, int Z1_lept1Id,
 		   TLorentzVector Z1_lept2, int Z1_lept2Id,
 		   TLorentzVector Z2_lept1, int Z2_lept1Id,
 		   TLorentzVector Z2_lept2, int Z2_lept2Id) ;
+
+  std::vector<std::pair<std::string,float> > computeNew(
+              const math::XYZTLorentzVector & Z1_lept1, int Z1_lept1Id,
+              const math::XYZTLorentzVector & Z1_lept2, int Z1_lept2Id,
+              const math::XYZTLorentzVector & Z2_lept1, int Z2_lept1Id,
+              const math::XYZTLorentzVector & Z2_lept2, int Z2_lept2Id,
+              const std::vector<math::XYZTLorentzVector> & jetP4s) ;
+
 
   float getKD() ;
   float getSuperKD() ;
